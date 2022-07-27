4b3c0b76675f53e4bda792f09ad7779a792259cb
ilyas rhrib <Ilyas.RhribExterne@ca-cdm.ma>
2022-07-27 10:02:26 +0100

    commit

--- src/main/java/ma/co/cdm/intertrans/IntertransBatchApplication.java
+++ src/main/java/ma/co/cdm/intertrans/IntertransBatchApplication.java
@@ -6,6 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class IntertransBatchApplication {
 
+	
+	
+	
+	
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(IntertransBatchApplication.class, args);
 	}
